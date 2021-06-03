76ec711eda9f6646206e0323aa61652e6eae9451
Natanael Lima <taelima1997@gmail.com>
2021-06-03 11:39:55 -0300

    update sentry config

--- App.tsx
+++ App.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import * as Sentry from 'sentry-expo';
-import Main from './src';
-import env from './env';
 
-Sentry.init({
-  dsn: env.SENTRY_DNS,
-  enableInExpoDevelopment: true,
-  debug: false,
-});
+import Main from './src';
 
 const App: React.FC = () => {
   return <Main />;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,20 @@ import { StatusBar } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { Provider } from 'react-redux';
 import { useFonts, Redressed_400Regular } from '@expo-google-fonts/redressed';
-
+import * as Sentry from 'sentry-expo';
 import { PersistGate } from 'redux-persist/integration/react';
 import Toast from 'react-native-toast-message';
+
 import Routes from './routes';
 import { persistor, store } from './store';
 import GlobalStyles from './styles';
+import env from '../env';
+
+Sentry.init({
+  dsn: env.SENTRY_DNS,
+  enableInExpoDevelopment: false,
+  debug: false,
+});
 
 const Main: React.FC = () => {
   const [fontsLoaded] = useFonts({
