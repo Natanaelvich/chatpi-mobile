3ebb34a9d651b9ac3c8e4ead363d69fd42f38efd
Natanael Lima <taelima1997@gmail.com>
2021-06-03 11:37:29 -0300

    update format date

--- src/components/DateParsed/index.tsx
+++ src/components/DateParsed/index.tsx
@@ -1,27 +1,30 @@
-import { formatRelative, parseISO } from 'date-fns';
+import { formatDistance } from 'date-fns/esm';
 import { pt } from 'date-fns/locale';
 import React, { useMemo } from 'react';
 
 import { Container, Title } from './styles';
 
 interface DateParsedProps {
-  date: Date;
+  date?: Date;
 }
 
 const DateParsed: React.FC<DateParsedProps> = ({ date }) => {
   const dateParsed = useMemo(() => {
-    try {
-      return formatRelative(date, new Date(), {
-        locale: pt,
-        addSuffix: true,
-      });
-    } catch (error) {
-      return null;
+    if (date) {
+      try {
+        return formatDistance(new Date(date), new Date(), {
+          locale: pt,
+          addSuffix: true,
+        });
+      } catch (error) {
+        return null;
+      }
     }
+    return '';
   }, [date]);
   return (
     <Container>
-      <Title>{dateParsed}</Title>
+      <Title>{dateParsed || ''}</Title>
     </Container>
   );
 };
