7cc9f05eb27805e2a6d8ae8b6156930afecd9f2d
Natanael Lima <taelima1997@gmail.com>
2021-06-04 11:46:28 -0300

    update funciton distance time

--- src/components/DateParsed/index.tsx
+++ src/components/DateParsed/index.tsx
@@ -1,3 +1,4 @@
+import { formatDistanceStrict, formatDistanceToNow } from 'date-fns';
 import { formatDistance } from 'date-fns/esm';
 import { pt } from 'date-fns/locale';
 import React, { useMemo } from 'react';
@@ -12,9 +13,10 @@ const DateParsed: React.FC<DateParsedProps> = ({ date }) => {
   const dateParsed = useMemo(() => {
     if (date) {
       try {
-        return formatDistance(new Date(date), new Date(), {
+        return formatDistanceToNow(new Date(date), {
           locale: pt,
           addSuffix: true,
+          includeSeconds: true,
         });
       } catch (error) {
         return null;
