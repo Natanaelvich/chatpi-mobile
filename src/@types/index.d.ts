75169fb0177e87267ee87550f243257bc88593ed
Natanael Lima <taelima1997@gmail.com>
2021-06-04 11:46:47 -0300

    resolve type tron

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+import { Reactotron } from 'reactotron-core-client';
+import { ReactotronReactNative } from 'reactotron-react-native';
+
 declare module '*.png';
+
+declare global {
+  interface Console {
+    tron: any;
+  }
+}
