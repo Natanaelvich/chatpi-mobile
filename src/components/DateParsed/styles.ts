5d9cd678d790eb7c84bdfcf6c47f55a6b600d6c9
Natanael Lima <taelima1997@gmail.com>
2021-06-04 12:19:39 -0300

    udpate styles dateParsed

--- src/components/DateParsed/styles.ts
+++ src/components/DateParsed/styles.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  max-width: 95%;
+`;
 export const Title = styled.Text`
   font-style: normal;
   font-weight: bold;
   font-size: 12px;
 
   color: #b5b5b5;
+  align-self: flex-end;
 `;
