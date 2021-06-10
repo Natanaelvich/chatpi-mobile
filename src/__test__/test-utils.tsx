c41dee1268818bf24c52753320b21f9da5af3747
Natanael Lima <taelima1997@gmail.com>
2021-06-09 23:44:28 -0300

    add renderWithRedux

---
+++ src/__test__/test-utils.tsx
@@ -0,0 +1,26 @@
+// test-utils.js
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../store/modules/rootReducer';
+// Import your own reducer
+
+function renderWithRedux(
+  ui,
+  {
+    initialState,
+    store = createStore(rootReducer, initialState),
+    ...renderOptions
+  } = {},
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from '@testing-library/react';
+// override render method
+export { renderWithRedux };
