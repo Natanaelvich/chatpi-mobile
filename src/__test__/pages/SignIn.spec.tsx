41027245d40fe3899ced533055741f7d12605365
Natanael Lima <taelima1997@gmail.com>
2021-06-10 22:06:36 -0300

    update signin tests

--- src/__test__/pages/SignIn.spec.tsx
+++ src/__test__/pages/SignIn.spec.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+
 import SingnIn from '../../pages/SingnIn';
-import { renderWithRedux } from '../test-utils';
+import { renderWithReduxAndTheme } from '../test-utils';
 
 const mockedNavigate = jest.fn();
 
@@ -9,21 +10,10 @@ jest.mock('@react-navigation/native', () => ({
   useNavigation: () => ({ navigate: mockedNavigate }),
 }));
 
-const mockDispatch = jest.fn();
-jest.mock('react-redux', () => ({
-  useSelector: jest.fn(),
-  useDispatch: () => mockDispatch,
-}));
-
 describe('SignIn page', () => {
   it('Should contains email/password inputs', async () => {
-    const { getByPlaceholderText } = renderWithRedux(<SingnIn />, {
-      initialState: {
-        user: {
-          signinError: false,
-          loadingSingin: false,
-        },
-      },
+    const { getByPlaceholderText } = renderWithReduxAndTheme(<SingnIn />, {
+      initialState: {},
     });
 
     expect(getByPlaceholderText('E-mail')).toBeTruthy();
