2d7eb00186285b792cd0dd27b76353495ba57d5f
Natanael Lima <taelima1997@gmail.com>
2021-06-10 22:06:16 -0300

    update renderWithReduxAndTheme

--- src/__test__/test-utils.tsx
+++ src/__test__/test-utils.tsx
@@ -1,12 +1,15 @@
 // test-utils.js
 import React from 'react';
-import { render as rtlRender } from '@testing-library/react';
+import { render as rtlRender } from '@testing-library/react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components/native';
 import rootReducer from '../store/modules/rootReducer';
+import light from '../styles/theme/light';
+
 // Import your own reducer
 
-function renderWithRedux(
+function renderWithReduxAndTheme(
   ui,
   {
     initialState,
@@ -15,12 +18,16 @@ function renderWithRedux(
   } = {},
 ) {
   function Wrapper({ children }) {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+      <Provider store={store}>
+        <ThemeProvider theme={light}>{children}</ThemeProvider>
+      </Provider>
+    );
   }
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
 
 // re-export everything
-export * from '@testing-library/react';
+export * from '@testing-library/react-native';
 // override render method
-export { renderWithRedux };
+export { renderWithReduxAndTheme };
