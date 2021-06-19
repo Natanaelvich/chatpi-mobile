14c8a40a3673151bbe370a227018abd24d63e80e
Natanael Lima <taelima1997@gmail.com>
2021-06-19 00:17:55 -0300

    update api default

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import env from '../../env';
 
 const api = axios.create({
-  baseURL: env.API_URL,
+  baseURL: 'https://api.pi.mundotech.dev',
 });
 
 export default api;
