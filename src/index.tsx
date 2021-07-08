44258c31ff2a54fd38bc1a23cf5e72903c19d87c
Natanael Lima <taelima1997@gmail.com>
2021-07-07 23:44:25 -0300

    add flux splashscreen

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { Platform, StatusBar, UIManager } from 'react-native';
+import { Platform, StatusBar, UIManager, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { Provider } from 'react-redux';
 import { useFonts, Redressed_400Regular } from '@expo-google-fonts/redressed';
-
+import * as SplashScreen from 'expo-splash-screen';
 import { PersistGate } from 'redux-persist/integration/react';
 import Toast from 'react-native-toast-message';
 import OneSignal from 'react-native-onesignal';
@@ -34,6 +34,36 @@ const Main: React.FC = () => {
     Redressed_400Regular,
   });
 
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare(): Promise<void> {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
@@ -45,6 +75,10 @@ const Main: React.FC = () => {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <GlobalStyles>
+            <View
+              onLayout={onLayoutRootView}
+              style={{ backgroundColor: 'red' }}
+            />
             <Routes />
             <Toast ref={ref => Toast.setRef(ref)} />
           </GlobalStyles>
