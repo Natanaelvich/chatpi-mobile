4a2a07e7b10fdf3b9976544663b2008736c41393
Natanael Lima <taelima1997@gmail.com>
2021-07-09 18:08:21 -0300

    add config middlewares redux-offline-queue

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-
+import {
+  offlineMiddleware,
+  suspendSaga,
+  consumeActionMiddleware,
+} from 'redux-offline-queue';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import { persistReducer, persistStore } from 'redux-persist';
 
 import rootReducer from './modules/rootReducer';
@@ -15,15 +18,19 @@ const persistConfig = {
   whitelist: ['user', 'messages', 'attendants', 'options'],
 };
 
+const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
+middlewares.push(offlineMiddleware());
+middlewares.push(suspendSaga(sagaMiddleware));
+middlewares.push(consumeActionMiddleware());
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const reactotronMiddleware = __DEV__ ? reactotron.createEnhancer() : null;
 
-const sagaMiddleware = createSagaMiddleware();
-
 const enhancer = __DEV__
-  ? compose(reactotronMiddleware, applyMiddleware(sagaMiddleware))
-  : applyMiddleware(sagaMiddleware);
+  ? compose(reactotronMiddleware, applyMiddleware(...middlewares))
+  : applyMiddleware(...middlewares);
 
 const store = createStore(persistedReducer, enhancer);
 
