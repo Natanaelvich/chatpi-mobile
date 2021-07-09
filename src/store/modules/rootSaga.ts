9daef2c2c5fbb259d46dbdc026c60aedd595c8b9
Natanael Lima <taelima1997@gmail.com>
2021-07-09 18:08:41 -0300

    add startWatchingNetworkConnectivity to rootSaga

--- src/store/modules/rootSaga.ts
+++ src/store/modules/rootSaga.ts
@@ -3,7 +3,13 @@ import { all } from 'redux-saga/effects';
 
 import user, { initCheck } from './user/sagas';
 import attendants from './attendants/sagas';
+import { startWatchingNetworkConnectivity } from './startWatchingNetworkConnectivity';
 
 export default function* rootSaga(): Generator {
-  return yield all([initCheck(), user, attendants]);
+  return yield all([
+    initCheck(),
+    startWatchingNetworkConnectivity(),
+    user,
+    attendants,
+  ]);
 }
