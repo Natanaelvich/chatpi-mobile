7a673cc3268dc4df7c43d8c5961c8f270da04cc1
Natanael Lima <taelima1997@gmail.com>
2021-07-16 18:04:44 -0300

    add singleUploadBackground

--- src/services/utils/uploadBackground.ts
+++ src/services/utils/uploadBackground.ts
@@ -1,41 +1,46 @@
 import Upload from 'react-native-background-upload';
 
-export function uploadBackground(): void {
+export async function singleUploadBackground(image: {
+  uri: string;
+}): Promise<void> {
+  // UPLOAD BACKGROUND
+  // https://stackoverflow.com/questions/31530200/node-multer-unexpected-field
+
+  const { mimeType } = await Upload.getFileInfo(
+    image.uri.replace('file://', ''),
+  );
+
   const options = {
-    url: 'https://myservice.com/path/to/post',
-    path: 'file://path/to/file/on/device',
-    method: 'POST',
-    type: 'raw',
+    url: 'https://api.pi.mundotech.dev/users/avatar',
+    path: image.uri.replace('file://', ''), // on ios remove replace
+    method: 'PATCH',
     maxRetries: 2, // set retry count (Android only). Default 2
     headers: {
-      'content-type': 'application/octet-stream', // Customize content-type
-      'my-custom-header': 's3headervalueorwhateveryouneed',
+      'content-type': mimeType,
+      authorization: `Bearer ${user?.token}`,
     },
     // Below are options only supported on Android
     notification: {
       enabled: true,
+      onProgressTitle: 'Atualizando foto de perfil',
+      onCompleteTitle: 'Sua foto de perfil foi atualizada 🖼',
+      onErrorTitle: 'Falha ao atualizar sua foto de perfil 😔',
     },
-    useUtf8Charset: true,
+    field: 'avatar',
+    type: 'multipart',
   };
 
-  Upload.startUpload(options)
-    .then(uploadId => {
-      console.log('Upload started');
-      Upload.addListener('progress', uploadId, data => {
-        console.log(`Progress: ${data.progress}%`);
-      });
-      Upload.addListener('error', uploadId, data => {
-        console.log(`Error: ${data.error}%`);
-      });
-      Upload.addListener('cancelled', uploadId, data => {
-        console.log(`Cancelled!`);
-      });
-      Upload.addListener('completed', uploadId, data => {
-        // data includes responseCode: number and responseBody: Object
-        console.log('Completed!');
-      });
-    })
-    .catch(err => {
-      console.log('Upload error!', err);
-    });
+  const uploadId = await Upload.startUpload(options);
+
+  Upload.addListener('progress', uploadId, data => {
+    console.log(`Progress: ${data.progress}%`);
+  });
+  Upload.addListener('error', uploadId, dataError => {
+    console.log(`Error: ${dataError.error}%`);
+    console.log(dataError);
+  });
+  Upload.addListener('cancelled', uploadId, data => {
+    console.log(`Cancelled!`);
+  });
+  Upload.addListener('completed', uploadId, data => {});
 }
