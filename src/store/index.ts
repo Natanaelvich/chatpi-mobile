e173e154f53bb168d080ec4d846ed8650be9abc6
Natanael Lima <taelima1997@gmail.com>
2021-07-17 11:10:57 -0300

    update with console.tron.createSagaMonitor()

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,30 +1,35 @@
+/* eslint-disable global-require */
 import { createStore, applyMiddleware, compose } from 'redux';
+
 import createSagaMiddleware from 'redux-saga';
-import { createWhitelistFilter } from 'redux-persist-transform-filter';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import {
   offlineMiddleware,
   suspendSaga,
   consumeActionMiddleware,
 } from 'redux-offline-queue';
-import { persistStore } from 'redux-persist';
+import { persistStore, REHYDRATE } from 'redux-persist';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 import persistReducers from './persistReducers';
 
-import { reactotron } from '../config/reactotronConfig';
-
-const reactotronMiddleware = __DEV__ ? reactotron.createEnhancer() : null;
+__DEV__ && require('../config/reactotronConfig');
 
 const middlewares = [];
-const sagaMiddleware = createSagaMiddleware();
-middlewares.push(offlineMiddleware());
+const sagaMiddleware = __DEV__
+  ? createSagaMiddleware({ sagaMonitor: console.tron.createSagaMonitor() })
+  : createSagaMiddleware();
+
+middlewares.push(
+  offlineMiddleware({
+    additionalTriggers: REHYDRATE,
+  }),
+);
 middlewares.push(suspendSaga(sagaMiddleware));
 middlewares.push(consumeActionMiddleware());
 
 const enhancer = __DEV__
-  ? compose(reactotronMiddleware, applyMiddleware(...middlewares))
+  ? compose(console.tron.createEnhancer(), applyMiddleware(...middlewares))
   : applyMiddleware(...middlewares);
 
 const store = createStore(persistReducers(rootReducer), enhancer);
