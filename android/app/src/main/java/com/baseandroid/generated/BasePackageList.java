bdb5a470c1be5ac3e942a30d97e89be9379be16c
Natanael Lima <taelima1997@gmail.com>
2021-07-30 23:21:39 -0300

    remove CameraPackage on BasePackageList

--- android/app/src/main/java/com/baseandroid/generated/BasePackageList.java
+++ android/app/src/main/java/com/baseandroid/generated/BasePackageList.java
@@ -9,7 +9,6 @@ public class BasePackageList {
     return Arrays.<Package>asList(
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.application.ApplicationPackage(),
-        new expo.modules.camera.CameraPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
