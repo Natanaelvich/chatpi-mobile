0e14d3f9b183387a5873a9f247276744e12a94f2
Natanael Lima <taelima1997@gmail.com>
2021-08-11 23:54:39 -0300

    add initial config refresh token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,49 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { Alert } from 'react-native';
 import { BASE_URL } from '../config';
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
+
+const isRefreshing = false;
+
+api.interceptors.response.use(
+  response => response,
+  (error: AxiosError) => {
+    const originalConfig = error.config;
+    if (error?.response?.status === 401) {
+      Alert.alert('Error', error.message);
+      // const chatpirefreshtoken = Cookies.get('chatpirefreshtoken');
+
+      // if (chatpirefreshtoken && !isRefreshing) {
+      //   isRefreshing = true;
+
+      //   return api
+      //     .post(`sessions/refresh-token?token=${chatpirefreshtoken}`)
+      //     .then(response => {
+      //       const { token, refresh_token } = response.data;
+
+      //       Cookies.set('chatpirefreshtoken', refresh_token);
+      //       Cookies.set('chatpitoken', token);
+
+      //       api.defaults.headers.Authorization = `Bearer ${token}`;
+      //       originalConfig.headers.Authorization = `Bearer ${token}`;
+
+      //       return api(originalConfig);
+      //     })
+      //     .catch(() => {
+      //       signOut();
+      //       return Promise.reject(error);
+      //     })
+      //     .finally(() => {
+      //       isRefreshing = false;
+      //     });
+      // }
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default api;
