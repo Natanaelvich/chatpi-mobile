014886404303e5951e88f9c880ec1646d72b519b
Natanael Lima <taelima1997@gmail.com>
2021-08-15 11:11:36 -0300

    add ignoreLogs

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 
 import Main from './src';
+
+LogBox.ignoreLogs([
+  'Animated: `useNativeDriver` was not specified.',
+  'Animated.event now requires a second argument for options',
+]);
 
 const App: React.FC = () => {
   return <Main />;
