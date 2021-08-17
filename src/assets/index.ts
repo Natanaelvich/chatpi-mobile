20a6990a6a1ca1a8e7ceb29373ed0587a8e52c51
Natanael Lima <taelima1997@gmail.com>
2021-08-16 22:54:25 -0300

    add assets main

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import downIcon from './chevronDown.png';
+import avatarProfile from './profile_avatar_placeholder.png';
+import typing from './typing.json';
+
+export default {
+  downIcon,
+  avatarProfile,
+  typing,
+};
