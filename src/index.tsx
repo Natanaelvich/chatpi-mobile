31cd0b17b2edffa7f00a1ea536ec139261433eea
Natanael Lima <taelima1997@gmail.com>
2021-08-19 23:51:09 -0300

    add DevSettings.addMenuItem Clear Data

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Platform, StatusBar, UIManager } from 'react-native';
+import { DevSettings, Platform, StatusBar, UIManager } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { Provider } from 'react-redux';
 import { useFonts, Redressed_400Regular } from '@expo-google-fonts/redressed';
@@ -8,9 +8,17 @@ import { useFonts, Redressed_400Regular } from '@expo-google-fonts/redressed';
 import { PersistGate } from 'redux-persist/integration/react';
 import Toast from 'react-native-toast-message';
 import OneSignal from 'react-native-onesignal';
+import AsyncStorage from '@react-native-community/async-storage';
 import Routes from './routes';
 import { persistor, store } from './store';
 import GlobalStyles from './styles';
+
+if (__DEV__) {
+  DevSettings.addMenuItem('Clear Data', async () => {
+    await AsyncStorage.clear();
+    DevSettings.reload();
+  });
+}
 
 const { ONESIGNAL_KEY } = process.env;
 OneSignal.setAppId(ONESIGNAL_KEY as string);
